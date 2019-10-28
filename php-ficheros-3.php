aa5d241677ec3405dca4926c9c42024cc98f18d9
CCOviedo <37302280+CCOviedo@users.noreply.github.com>
2019-10-28 11:33:14 +0100

    Add files via upload

---
+++ php-ficheros-3.php
@@ -0,0 +1,16 @@
+<?php
+	function funciones($ruta ,$fichero){
+		$hoy = date("Ymj");
+		$rutaCompleta = $ruta . '\\' . $hoy;
+		
+		if(!file_exists($rutaCompleta)){
+			mkdir($rutaCompleta, 0777, true);
+			copy($fichero, $rutaCompleta ."\\". $fichero . ".modificado");
+		}else{
+			opendir($hoy);
+			copy($fichero, $rutaCompleta ."\\". $fichero . ".modificado");
+		}
+	}
+	
+	funciones("C:\\xampp\\htdocs", "elquijote.txt");
+?>
